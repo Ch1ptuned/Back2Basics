3d233104ab68e68b5801e90a2547a50d307147c2
Ch1ptuned <justin.konst@gmail.com>
2024-12-08 21:22:28 +0100

    Camera, Moving and Jumping

--- Assets/Scripts/Player Camera.cs
+++ Assets/Scripts/Player Camera.cs
@@ -21,7 +21,7 @@ public class PlayerCamera : MonoBehaviour
     private void Update()
     {
         //rotate orientation
-        Vector3 viewDir = player.position - new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
         orientation.forward = viewDir.normalized;
 
         //rotate player object
